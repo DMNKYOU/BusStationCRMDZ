6cb00d67488786b2bcbd498fe2cdc9fde6bf4351
DmitriyZalesin <zalesindmitriy@gmail.com>
2021-10-13 23:49:54 +0300

    add base repository interface and class for CRUD operations

---
+++ BusStationCRM.DAL/Repositories/BaseRepository.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusStationCRM.DAL.Contexts;
+using BusStationCRM.DAL.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace BusStationCRM.DAL.Repositories
+{
+    //Base class for repositories
+    public class BaseRepository<TEntity> : IRepositoryAsync<TEntity> where TEntity : class
+    {
+        protected readonly ApplicationDbContext _context;
+        protected readonly DbSet<TEntity> _entities;
+        public BaseRepository(ApplicationDbContext context)
+        {
+            _context = context;
+            _entities = context.Set<TEntity>();
+        }
+
+        public IEnumerable<TEntity> Find(Func<TEntity, bool> predicate)
+        {
+            return _entities.AsEnumerable().Where(predicate).ToList();
+        }
+
+        public async Task<TEntity> GetAsync(int id) ////////////////////////////////////should be override
+        {
+            return await _entities.FindAsync(id);
+        }
+
+        public async Task<List<TEntity>> GetAllAsync()/////////////////////////// should be override
+        {
+            return await _entities.ToListAsync();
+        }
+
+        public async Task CreateAsync(TEntity item)
+        {
+            await _entities.AddAsync(item);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(TEntity item)
+        {
+            _context.Entry(item).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var entity = await _entities.FindAsync(id);
+
+            if (entity == null)
+            {
+                throw new ArgumentException($"Error! {typeof(TEntity)} not deleted, because not found!");
+            }
+
+            _entities.Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
